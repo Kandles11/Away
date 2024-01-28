688e754853d0b8fad03018ea697d6862e6f13ad0
Mason Thomas <Kandleseatwicks@gmail.com>
2024-01-27 22:39:14 -0600

    add new tabs, icons, change index tab

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,33 +1,77 @@
 import { Link, Tabs } from 'expo-router'
 import { Pressable } from 'react-native'
 import { Text } from 'tamagui'
+import { Home, Plane, ScanBarcode, Luggage, UserCircle2} from '@tamagui/lucide-icons'
+import { YStack } from 'tamagui'
+
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: 'red',
+        tabBarStyle: {height: 90}
       }}
     >
       <Tabs.Screen
         name='index'
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <Text>Hello!</Text>,
+          title: '',
+          headerStyle: {
+            elevation: 0,
+            shadowOpacity: 0,
+            borderBottomWidth: 0,
+            backgroundColor: 'transparent',
+          },
+          tabBarIcon: () => {
+            return <YStack alignItems="center" >
+              <Home/>
+              <Text fontSize={10}>Home</Text>
+              </YStack>;
+          },
           headerRight: () => (
             <Link href='/modal' asChild>
               <Pressable>
-                <Text>Hello!</Text>
+                <UserCircle2 margin={15}/>
               </Pressable>
             </Link>
           ),
         }}
       />
       <Tabs.Screen
-        name='two'
+        name='travel'
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <Text>Hello!</Text>,
+          title: '',
+          tabBarIcon: () => {
+            return <YStack alignItems="center" >
+              <Plane/>
+              <Text fontSize={10}>Travel</Text>
+              </YStack>;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name='pack'
+        options={{
+          title: '',
+          tabBarIcon: () => {
+            return <YStack alignItems="center" >
+              <Luggage/>
+              <Text fontSize={10}>Pack</Text>
+              </YStack>;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name='scan'
+        options={{
+          title: '',
+          tabBarIcon: () => {
+            return <YStack alignItems="center" >
+              <ScanBarcode/>
+              <Text fontSize={10}>Scan</Text>
+              </YStack>;
+          },
         }}
       />
     </Tabs>
