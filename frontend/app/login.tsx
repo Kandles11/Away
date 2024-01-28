996ddd602edf122e1f7f20a2796c0fdde5286225
Jason Antwi-Appah <hey@jasonaa.me>
2024-01-28 05:46:57 -0600

    Implement login screen

--- frontend/app/login.tsx
+++ frontend/app/login.tsx
@@ -1,21 +1,36 @@
 import { Link, Stack } from 'expo-router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
-import { View, Text, Form, Input, YStack, Button } from 'tamagui'
-import { useRegisterMutation } from '../hooks'
+import { View, Text, Form, Input, YStack, Button, ToggleGroup } from 'tamagui'
+import { useLoginMutation, useRegisterMutation } from '../hooks'
 
+const AuthType = {
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+} as const
+const INITIAL_AUTH_TYPE = AuthType.REGISTER
 export default function NotFoundScreen() {
   const [email, setEmail] = useState('')
   const [name, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const register = useRegisterMutation()
+  const login = useLoginMutation()
+  const [currentAuthType, setCurrentAuthType] = useState<typeof AuthType[keyof typeof AuthType]>(INITIAL_AUTH_TYPE)
+  useEffect(() => {
+    console.log({ currentAuthType })
+  }, [currentAuthType])
+  
   return (
     <>
       <Stack.Screen options={{ title: 'Login!' }} />
       <View margin={10}>
       <Form onSubmit={()=>{
-        console.log({username: name, password})
-        register.mutate({requestBody:{name, email, password}})
+        console.log({ email, name, password})
+        if (currentAuthType === AuthType.LOGIN) {
+          login.mutate({requestBody:{ email, password}})
+        } else {
+          register.mutate({requestBody:{name, email, password}})
+        }
       }}>
       <YStack
       minHeight={250}
@@ -24,7 +39,16 @@ export default function NotFoundScreen() {
     >
       {register.isError && <Text>Error: {register.error?.message}</Text>}
       {register.isPending && <Text>Loading....</Text>}
-      <Input flex={1} value={name} onChangeText={(e)=>setUsername(e)} placeholder={`Username`} />
+      {}
+      <ToggleGroup loop={false} disableDeactivation defaultValue={INITIAL_AUTH_TYPE} value={currentAuthType} onValueChange={(d)=>{
+        console.log({d})
+        const v = d
+        setCurrentAuthType(v as any)}
+        } type="single">
+      <ToggleGroup.Item value={AuthType.LOGIN}><Text>Login</Text></ToggleGroup.Item>
+      <ToggleGroup.Item value={AuthType.REGISTER}><Text>Register</Text></ToggleGroup.Item>
+    </ToggleGroup>
+      {currentAuthType === AuthType.REGISTER&&<Input flex={1} value={name} onChangeText={(e)=>setUsername(e)} placeholder={`Username`} />}
       <Input flex={1} value={email} onChangeText={(e)=>setEmail(e)} placeholder={`Email`} />
       <Input flex={1} value={password} onChangeText={(e)=>setPassword(e)} placeholder={`Password`} />
       <Form.Trigger asChild>
