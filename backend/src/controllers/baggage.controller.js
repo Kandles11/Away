884de6a8c210ce4258270c1b1a453e84b6bcd121
Mason Thomas <Kandleseatwicks@gmail.com>
2024-01-27 18:04:37 -0600

    Add baggage update and retrieval endpoints

--- backend/src/controllers/baggage.controller.js
+++ backend/src/controllers/baggage.controller.js
@@ -13,6 +13,45 @@ const createBaggage = catchAsync(async (req, res) => {
   }
 });
 
+const updateBaggage = catchAsync(async (req, res) => {
+  const { tagData, claimed } = req.body;
+
+  const filter = { tagData };
+  const update = { claimed };
+
+  try {
+    const baggage = await Baggage.findOneAndUpdate(filter, update, { new: true });
+    res.status(200).json(baggage);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+const getBaggage = catchAsync(async (req, res) => {
+  const { tagData } = req.body;
+
+  try {
+    const baggage = await Baggage.findOne({ tagData }).exec();
+    res.status(200).json(baggage);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+const getBaggagesByUser = catchAsync(async (req, res) => {
+  const { user } = req.body;
+
+  try {
+    const baggages = await Baggage.find({ user }).exec();
+    res.status(200).json(baggages);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = {
   createBaggage,
+  updateBaggage,
+  getBaggage,
+  getBaggagesByUser,
 };
--- backend/src/routes/v1/baggage.route.js
+++ backend/src/routes/v1/baggage.route.js
@@ -4,5 +4,8 @@ const baggageController = require('../../controllers/baggage.controller');
 const router = express.Router();
 
 router.route('/').post(baggageController.createBaggage);
+router.route('/').patch(baggageController.updateBaggage);
+router.route('/').get(baggageController.getBaggage);
+router.route('/bags/').get(baggageController.getBaggagesByUser);
 
 module.exports = router;
