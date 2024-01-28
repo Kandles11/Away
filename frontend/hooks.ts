e831318a06581ab3c31887afe6531135b22cdf82
Jason Antwi-Appah <hey@jasonaa.me>
2024-01-28 01:24:51 -0600

    Set up global state management

--- frontend/hooks.ts
+++ frontend/hooks.ts
@@ -1,13 +1,24 @@
-import { OpenAPI } from '../generated/client/';
+import { AuthTokens, OpenAPI, User } from '../generated/client/';
 import { APIClient } from '../generated/client/APIClient';
+import { create } from 'zustand'
+
 const c = new APIClient()
 
+// TODO: figure out zustand store persistence
+// TODO: use react query for all data fetching
+const initialState = {
+    authToken: undefined as string | undefined,
+    user: undefined as User | undefined,
+    tokens: undefined as AuthTokens | undefined,
+}
+
+type State = typeof initialState
+
+export const useAppStore = create<State>((set) => (initialState))
+
 const getToken = async () => {
-    // TODO: (@jasonappah) implement this. should pull from whatever state management solution i set up
-    return 'SOME_TOKEN';
+    return Promise.resolve(useAppStore.getState().authToken || '')
 };
-
-
 
 OpenAPI.TOKEN = getToken;
 
